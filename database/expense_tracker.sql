37476cdcb671ce3836eb1437117e60efc95b6d06
jpcyoyoyo <46081337+jpcyoyoyo@users.noreply.github.com>
2024-10-08 10:51:42 +0800

    Correcting the file inside database
    0.0.8.1
    
    MINOR CHANGES:
    - Correcting the file inside database from user_accounts to expense_tracker
    
    - jpcyoyoyo
    - LABAN LANG GUYS

--- database/expense_tracker.sql
+++ database/expense_tracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2024 at 06:27 PM
+-- Generation Time: Oct 07, 2024 at 06:48 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
